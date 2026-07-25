fa7ff5108db43bef6c21027a673acf6966997fe8
yaqubovm168-hub <yaqubovm168@gmail.com>
2026-07-25 06:07:53 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask, request
 TOKEN = os.environ["BOT_TOKEN"]
 API_URL = f"https://api.telegram.org/bot{TOKEN}"
 
-app = Flask(name)
+app aapp = Flask(__name__)
 
 # Kino kodi va Telegram video file_id
 MOVIES = {
